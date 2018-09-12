9b4cb859bf893b96b3456c016888f1685afed424
Corry TERBLANCHE <cterblan@e4r13p6.wethinkcode.co.za>
2018-09-12 10:40:44 +0200

    Fixed Time Sorting Not working

--- src/ftls_sort.c
+++ src/ftls_sort.c
@@ -6,7 +6,7 @@
 /*   By: cterblan <cterblan@student.wethinkcode>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 15:25:08 by cterblan          #+#    #+#             */
-/*   Updated: 2018/09/12 09:41:43 by cterblan         ###   ########.fr       */
+/*   Updated: 2018/09/12 10:34:05 by cterblan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ftls_sort(t_lsflags *f, t_lslink **l)
 {
 	if (f->r == 0 && f->t == 0)
 		ftls_sort_name(l);
+	if (f->t == 1)
+		ftls_sort_time(l);
 	/*else if (f->r == 1 && f->t == 0)
 		ftls_rsort_name(l);*/
 }
