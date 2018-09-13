b0817f752b167983cd7c9268e15a56ea983561f4
Corry TERBLANCHE <cterblan@e4r13p6.wethinkcode.co.za>
2018-09-13 13:46:24 +0200

    fixed random value using -R

--- src/ftls_recall.c
+++ src/ftls_recall.c
@@ -6,7 +6,7 @@
 /*   By: cterblan <cterblan@student.wethinkcode>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/13 12:50:23 by cterblan          #+#    #+#             */
-/*   Updated: 2018/09/13 13:07:33 by cterblan         ###   ########.fr       */
+/*   Updated: 2018/09/13 13:45:41 by cterblan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	ftls_recall(char *path, t_lslink *l, t_lsflags *f)
 	tmp = l;
 	while (tmp)
 	{
-		if (ft_isdir(tmp->name) && (0 != ft_strcmp("..", tmp->name) &&
+		if (ft_isdir(ft_strjoin(path, tmp->name)) &&
+			(0 != ft_strcmp("..", tmp->name) &&
 			0 != ft_strcmp(".", tmp->name)))
 			ftls_read_dir(ft_strjoin(path, tmp->name), f);
 		tmp = tmp->next;
