33349cb5cae60a336ef495390c9a7298fd9e1aad
Corry TERBLANCHE <cterblan@e4r13p6.wethinkcode.co.za>
2018-09-14 09:08:42 +0200

    fixed -R

--- src/ftls_recall.c
+++ src/ftls_recall.c
@@ -6,7 +6,7 @@
 /*   By: cterblan <cterblan@student.wethinkcode>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/13 12:50:23 by cterblan          #+#    #+#             */
-/*   Updated: 2018/09/14 07:47:00 by cterblan         ###   ########.fr       */
+/*   Updated: 2018/09/14 09:08:00 by cterblan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ void	ftls_recall(char *path, t_lslink *l, t_lsflags *f)
 	while (tmp)
 	{
 		if (ft_isdir(fre = ft_strjoin(path, tmp->name)) &&
-			(0 != ft_strcmp("..", tmp->name) &&
+			(f->a == 0) && tmp->name[0] != '.')
+			ftls_read_dir(fre, f);
+		else if (ft_isdir(fre = ft_strjoin(path, tmp->name)) &&
+			f->a ==1 && (0 != ft_strcmp("..", tmp->name) &&
 			0 != ft_strcmp(".", tmp->name)))
 			ftls_read_dir(fre, f);
 		tmp = tmp->next;
